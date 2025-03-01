6547efe3ef5b0e0aba8b46641f5546c7c2fb435a
jks-sn <konstantinberkaev@gmail.com>
2025-03-02 00:01:58 +0700

    Add stub for time library to work in lab2

---
+++ tests/lab1/time_stubs/time_stub.c
@@ -0,0 +1,10 @@
+//tests/lab1/logger/stubs/time_stub.c
+
+#include <time.h>
+
+/* Stub implementation for lab1.
+ * Returns a fixed timestamp (e.g., 0) so that get_time is defined.
+ */
+time_t get_time(void) {
+    return 1609459200;
+}
